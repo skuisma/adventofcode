9bd62716405c5c8b6fd1e4150d334825d65b98e4
sampo.kuisma <sampo.kuisma@accenture.com>
2020-01-13 10:50:16 +0200

    Day4, first guest

--- src/aoc2019/Day04.java
+++ src/aoc2019/Day04.java
@@ -1,17 +1,18 @@
 package aoc2019;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 public class Day04 {
-    List<Integer> passwords = new ArrayList<>();
-
     public static void main(String[] args) {
         Day04 day = new Day04();
-        day.solve(134792, 675810);
+        day.solve2(134792, 675810);
     }
 
     private void solve(int startNumber, int endNumber) {
+        List<Integer> passwords = new ArrayList<>();
         for (int i = startNumber; i <= endNumber; i++) {
             String number = String.valueOf(i);
             int previousNumber = 0;
@@ -28,6 +29,45 @@ public class Day04 {
                 }
                 previousNumber = currentNumber;
             }
+            if (adjacentPair && increasingNumbers) {
+                passwords.add(i);
+            }
+        }
+        System.out.println(passwords.size());
+    }
+
+    private void solve2(int startNumber, int endNumber) {
+        List<Integer> passwords = new ArrayList<>();
+        for (int i = startNumber; i <= endNumber; i++) {
+            String number = String.valueOf(i);
+            int previousNumber = 0;
+            Map<Integer,Integer> adjacentPairs = new HashMap<>();
+            boolean adjacentPair = true;
+            boolean increasingNumbers = true;
+            for (int j = 0; j < 6; j++) {
+                int currentNumber = (int) number.charAt(j);
+                if (previousNumber == currentNumber) {
+                    int count = 0;
+                    for (int k = j; k >= 0 ; k--) {
+                        if(number.charAt(k) == currentNumber) {
+                            count++;
+                        }
+                    }
+                    adjacentPairs.put(currentNumber, count);
+                }
+                if (previousNumber > currentNumber) {
+                    increasingNumbers = false;
+                    break;
+                }
+                previousNumber = currentNumber;
+            }
+            for (Map.Entry<Integer, Integer> entry : adjacentPairs.entrySet()) {
+                Integer v = entry.getValue();
+                if (v % 2 != 0) {
+                    adjacentPair = false;
+                }
+            }
+            //System.out.printf("%d - %b | %b%n", i, adjacentPair, increasingNumbers);
             if (adjacentPair && increasingNumbers) {
                 passwords.add(i);
             }
