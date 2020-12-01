d489d1a9f204a7ea3327bee0ef4e8d565a7016ac
Sampo Kuisma <sampo.kuisma@accenture.com>
2020-12-01 23:58:14 +0200

    cleaner version

--- src/aoc2020/Day01.java
+++ src/aoc2020/Day01.java
@@ -8,30 +8,28 @@ public class Day01 {
 
     public static void main(String[] args) {
         Day01 day = new Day01();
-        day.solve1();
+        day.solve();
     }
 
-    private void solve1() {
-        String datapath = "resources/aoc2020/day1-1.txt";
-        FileReader fr = new FileReader(datapath);
-        List<Integer> data =  fr.readIntFile();
-        data.forEach(row -> {
-            data.forEach(row2 -> {
-                if (row != row2 && row + row2 ==2020){
-                    System.out.println("Part 1 : " + row * row2);
-                }
-            });
-        });
+    private void solve() {
+        String dataPath = "resources/aoc2020/day1-1.txt";
+        FileReader fr = new FileReader(dataPath);
+        List<Integer> data = fr.readIntFile();
 
-        for (Integer integer : data) {
-            for (Integer integer2 : data) {
-                for (Integer integer3 : data) {
-                    if (integer != integer2 && integer != integer3 && integer2 != integer3 && integer + integer2 + integer3 == 2020){
-                        System.out.println("Part 2 :" + integer * integer2 * integer3);
-                        break;
-                    }
-                }
-            }
-        }
+        int part1 = data.stream()
+                .map(value -> 2020 - value)
+                .filter(value -> data.contains(value))
+                .reduce(1, (integer, integer2) -> integer * integer2);
+        System.out.println(part1);
+
+        int part2 = data.stream()
+                .map(value -> 2020 - value)
+                .flatMap(val1 -> data.stream()
+                        .map(val2 -> val1 - val2)
+                        .filter(val3 -> data.contains(val3))
+                )
+                .distinct()
+                .reduce(1, (integer, integer2) -> integer * integer2);
+        System.out.println(part2);
     }
 }
